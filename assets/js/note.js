eeaa208ee6d2f441dc4eba2060d65d1a417f6d63
Annette von Stockhausen <annette@vonstockhausen.eu>
2020-02-04 14:32:40 +0100

    Layout updated
    Fonts, CSS and JS all local installed

--- assets/js/note.js
+++
@@ -1,9 +0,0 @@
-   var sup = document.getElementById("ss-1");
-    sup.onclick = function(event) {
-
-      var footnoteText = this.parentElement.parentElement.getElementsByClassName('note-content');
-
-      footnoteText[0].style.display = 'block';
-
-      event.preventDefault();
-    };
